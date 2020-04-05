dab516e090d0bcf7dc196f7f3456cf7d9f3aa708
Sh3r4 <sh3r4@timms.io>
2020-04-05 13:21:03 +1000

    start framework for dynamic behaviour

---
+++ SnaffCore/Definitions/Classifier.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace SnaffCore.Definitions
+{
+    public class Classifier
+    {
+        // define a way to chain classifiers together
+        public string ClassifierName { get; set; }
+        public MatchAction MatchAction { get; set; }
+        public string RelayTarget { get; set; }
+        
+        // define the behaviour of this classifier
+        public MatchLoc MatchLocation { get; set; }
+        public MatchListType WordListType { get; set; }
+        public List<string> WordList { get; set; }
+        
+        // define the severity of this classification
+        public Triage Triage { get; set; }
+    }
+    
+    
+    public enum MatchLoc
+    {
+        FilePath,
+        FileName,
+        FileExtension,
+        FileContent
+    }
+
+    public enum MatchListType
+    {
+        Exact,
+        Contains,
+        Regex
+    }
+
+    public enum MatchAction
+    {
+        Discard,
+        Snaffle,
+        Relay
+    }
+
+    public enum Triage
+    {
+        Black,
+        Green,
+        Yellow,
+        Red
+    }
+}

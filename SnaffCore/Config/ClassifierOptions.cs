dd481296bb926f87ac161650fb4ddf3b6233744c
Mike Loss <mike@mikeloss.net>
2020-04-11 05:42:41 -0700

    more rules

--- SnaffCore/Config/ClassifierOptions.cs
+++ SnaffCore/Config/ClassifierOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Classifiers;
 
 namespace SnaffCore.Config
@@ -268,12 +269,12 @@ namespace SnaffCore.Config
                         ".exports",
                         ".functions",
                         ".extra",
-                        "bash_history",
-                        "zsh_history",
-                        "sh_history",
+                        ".bash_history",
+                        ".zsh_history",
+                        ".sh_history",
                         "zhistory",
-                        "mysql_history",
-                        "psql_history",
+                        ".mysql_history",
+                        ".psql_history",
                         ".pgpass",
                         ".irb_history",
                         ".dbeaver-data-sources.xml",
@@ -336,10 +337,11 @@ namespace SnaffCore.Config
                     {
                         "password=",
                         "password =",
-                        "cpassword"
+                        "cpassword",
+                        "NVRAM config last updated"
                     },
                 },
-                /*
+                
                 new Classifier()
                 {
                     ClassifierName = "CodeGrepExtExact",
@@ -383,8 +385,15 @@ namespace SnaffCore.Config
                     WordList = new List<string>()
                     {
                         "password",
+                        "new OleDbConnection(", // asp.net
+                        "\\.createConnection\\(.*",
+                        "mysql_connect[[:space:]]*\\(.*\\$.*\\)",  // php
+                        "mysql_pconnect[[:space:]]*\\(.*\\$.*\\)",  // php
+                        "mysql_change_user[[:space:]]*\\(.*\\$.*\\)", // php
+                        "pg_connect[[:space:]]*\\(.*\\$.*\\)", // php
+                        "pg_pconnect[[:space:]]*\\(.*\\$.*\\)" // php
                     },
-                },*/
+                },
                 
                 new Classifier()
                 {
@@ -464,6 +473,7 @@ namespace SnaffCore.Config
                         "sqlConnectionString=\"",
                         "validationKey=",
                         "decryptionKey=",
+                        "NVRAM config last updated"
                     },
                 },
                 new Classifier()
@@ -497,115 +507,59 @@ namespace SnaffCore.Config
                         "PRIVATE KEY----",
                     },
                 },
+                new Classifier()
+                {
+                    ClassifierName = "KeepTokensRegexRed",
+                    EnumerationScope = EnumerationScope.ContentsEnumeration,
+                    MatchLocation = MatchLoc.FileContentAsString,
+                    WordListType = MatchListType.Regex,
+                    MatchAction = MatchAction.Snaffle,
+                    Triage = Triage.Red,
+                    WordList = new List<string>()
+                    {
+                        // thanks to sshgit
+                        "-----BEGIN [EC|RSA|DSA|OPENSSH] PRIVATE KEY----", // ssh priv key
+                        "(A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16}", // aws access key
+                        "((\\\"|'|`)?((?i)aws)?_?((?i)account)_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)[0-9]{4}-?[0-9]{4}-?[0-9]{4}(\\\"|'|`)?)", // aws account id
+                        "((\\\"|'|`)?((?i)aws)?_?((?i)secret)_?((?i)access)?_?((?i)key)?_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=\\\\s{0,50}(\\\"|'|`)?[A-Za-z0-9/+=]{40}(\\\"|'|`)?)", // aws secret id
+                        "((\\\"|'|`)?((?i)aws)?_?((?i)session)?_?((?i)token)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)[A-Za-z0-9/+=]{16,}(\\\"|'|`)?)", // aws session token
+                        "(?i)artifactory.{0,50}(\\\"|'|`)?[a-zA-Z0-9=]{112}(\\\"|'|`)?",//Artifactory
+                        "(?i)codeclima.{0,50}(\\\"|'|`)?[0-9a-f]{64}(\\\"|'|`)?",//CodeClimate
+                        "EAACEdEose0cBA[0-9A-Za-z]+",//Facebook access token
+                        "((\\\"|'|`)?type(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?service_account(\\\"|'|`)?,?)",//Google (GCM) S"rvice account
+                        "(?:r|s)k_[live|test]_[0-9a-zA-Z]{24}",//Stripe API key
+                        "[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com",//Google OAuth Key
+                        "AIza[0-9A-Za-z\\-_]{35}",//Google Cloud API Key
+                        "ya29\\.[0-9A-Za-z\\-_]+",//Google OAuth Access Token
+                        "sk_[live|test]_[0-9a-z]{32}",//Picatic API key
+                        "sq0atp-[0-9A-Za-z\\-_]{22}",//Square Access Token
+                        "sq0csp-[0-9A-Za-z\\-_]{43}",//Square OAuth Secret
+                        "access_token\\$production\\$[0-9a-z]{16}\\$[0-9a-f]{32}",//PayPal/Braintree Access Token
+                        "amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",//Amazon MWS Auth Token
+                        "SK[0-9a-fA-F]{32}",//Twilo API Key
+                        "key-[0-9a-zA-Z]{32}",//MailGun API Key
+                        "[0-9a-f]{32}-us[0-9]{12}",//MailChimp API Key
+                        "sshpass -p.*['|\\\"]",//SSH Password
+                        "(https\\://outlook\\.office.com/webhook/[0-9a-f-]{36}\\@)",//Outlook team
+                        "(?i)sauce.{0,50}(\\\"|'|`)?[0-9a-f-]{36}(\\\"|'|`)?",//Sauce Token
+                        "(xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",//Slack Token
+                        "https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}",//Slack Webhook
+                        "(?i)sonar.{0,50}(\\\"|'|`)?[0-9a-f]{40}(\\\"|'|`)?",//SonarQube Docs API Key
+                        "(?i)hockey.{0,50}(\\\"|'|`)?[0-9a-f]{32}(\\\"|'|`)?",//HockeyApp
+                        "([\\w+]{1,24})(://)([^$<]{1})([^\\s\";]{ 1,}):([^$<]{ 1})([^\\s\";]{1,})@[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\[a-zA-Z0-9()]{1,24}([\"\\s] +)",//Username and password in URI
+                        "oy2[a-z0-9]{43}",//NuGet API Key
+                        "[Aa][Ww][Ss][_\\-\\.]?[Ss][Ee][Cc][Rr][Ee][Tt][_\\-\\.]?[Kk][Ee][Yy]",
+                        "[Aa][Ww][Ss][_\\-\\.]?[Kk][Ee][Yy]",
+                        "[_\\-\\.]?[Aa][Pp][Ii][_\\-\\.]?[Kk][Ee][Yy]",
+                        "[_\\-\\.][Oo][Aa][Uu][Tt][Hh][[:space:]]*=",
+                        "(client_secret|CLIENT_SECRET)",
+                        "[Ss][Ee][Cc][Rr][Ee][Tt][_\\-\\.]?([Kk][Ee][Yy])?[[:space:]]*=",
+                        "[_\\-\\.][Pp][Aa][Ss][Ss][Ww][Oo]?[Rr]?[Dd][[:space:]]*=[[:space:]]*[\'\"\\][^\'\"].....*",
+                        "-----BEGIN( RSA| OPENSSH| DSA| EC| PGP)? PRIVATE KEY( BLOCK)?-----",
+                    }
+                },
             };
-            // regexes
-
-            /*public class Regexes
-            {
-
-                Regex privkey = new Regex(@"-----BEGIN [EC|RSA|DSA|OPENSSH] PRIVATE KEY----", 
-                    RegexOptions.Compiled);
-
-                Regex awsaccesskey = new Regex(@"(A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16}",
-                    RegexOptions.Compiled);
-
-                Regex awsaccountid = new Regex(@"((\\\"|'|`)?((?i)aws)?_?((?i)account)_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[0-9]{4}-?[0-9]{4}-?[0-9]{4}(\\\"|'|`)?)",
-                    RegexOptions.Compiled);
-
-                Regex awssecretkey = new Regex(@"((\\\"|'|`)?((?i)aws)?_?((?i)secret)_?((?i)access)?_?((?i)key)?_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[A-Za-z0-9/+=]{40}(\\\"|'|`)?)",
-                    RegexOptions.Compiled);
-                /*
-
-            - part: 'contents'
-            regex: "((\\\"|'|`)?((?i)aws)?_?((?i)access)_?((?i)key)?_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?(A3T[A-Z0-9]|AKIA|AGPA|AIDA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16}(\\\"|'|`)?)"
-            name: 'AWS Access Key ID'
-            - part: 'contents'
-            regex: "((\\\"|'|`)?((?i)aws)?_?((?i)account)_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[0-9]{4}-?[0-9]{4}-?[0-9]{4}(\\\"|'|`)?)"
-            name: 'AWS Account ID'
-            - part: 'contents'
-            regex: "((\\\"|'|`)?((?i)aws)?_?((?i)secret)_?((?i)access)?_?((?i)key)?_?((?i)id)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[A-Za-z0-9/+=]{40}(\\\"|'|`)?)"
-            name: 'AWS Secret Access Key'
-            - part: 'contents'
-            regex: "((\\\"|'|`)?((?i)aws)?_?((?i)session)?_?((?i)token)?(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?[A-Za-z0-9/+=]{16,}(\\\"|'|`)?)"
-            name: 'AWS Session Token'
-            - part: 'contents'
-            regex: "(?i)artifactory.{0,50}(\\\"|'|`)?[a-zA-Z0-9=]{112}(\\\"|'|`)?"
-            name: 'Artifactory'
-            - part: 'contents'
-            regex: "(?i)codeclima.{0,50}(\\\"|'|`)?[0-9a-f]{64}(\\\"|'|`)?"
-            name: 'CodeClimate'
-            - part:  'contents'
-            regex: 'EAACEdEose0cBA[0-9A-Za-z]+'
-            name: 'Facebook access token'
-            - part: 'contents'
-            regex: "((\\\"|'|`)?type(\\\"|'|`)?\\\\s{0,50}(:|=>|=)\\\\s{0,50}(\\\"|'|`)?service_account(\\\"|'|`)?,?)"
-            name: 'Google (GCM) Service account'
-            - part:  'contents'
-            regex: '(?:r|s)k_[live|test]_[0-9a-zA-Z]{24}'
-            name: 'Stripe API key'
-            - part:  'contents'
-            regex: '[0-9]+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com'
-            name: 'Google OAuth Key'
-            - part: 'contents'
-            regex: 'AIza[0-9A-Za-z\\-_]{35}'
-            name: 'Google Cloud API Key'
-            - part: 'contents'
-            regex: 'ya29\\.[0-9A-Za-z\\-_]+'
-            name: 'Google OAuth Access Token'
-            - part:  'contents'
-            regex: 'sk_[live|test]_[0-9a-z]{32}'
-            name: 'Picatic API key'
-            - part:  'contents'
-            regex: 'sq0atp-[0-9A-Za-z\-_]{22}'
-            name: 'Square Access Token'
-            - part:  'contents'
-            regex: 'sq0csp-[0-9A-Za-z\-_]{43}'
-            name: 'Square OAuth Secret'
-            - part:  'contents'
-            regex: 'access_token\$production\$[0-9a-z]{16}\$[0-9a-f]{32}'
-            name: 'PayPal/Braintree Access Token'
-            - part:  'contents'
-            regex: 'amzn\.mws\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
-            name: 'Amazon MWS Auth Token'
-            - part:  'contents'
-            regex: 'SK[0-9a-fA-F]{32}'
-            name: 'Twilo API Key'
-            - part:  'contents'
-            regex: 'key-[0-9a-zA-Z]{32}'
-            name: 'MailGun API Key'
-            - part:  'contents'
-            regex: '[0-9a-f]{32}-us[0-9]{12}'
-            name: 'MailChimp API Key'
-            - part:  'contents'
-            regex: "sshpass -p.*['|\\\"]"
-            name: 'SSH Password'
-            - part: 'contents'
-            regex: '(https\\://outlook\\.office.com/webhook/[0-9a-f-]{36}\\@)'
-            name: 'Outlook team'
-            - part: 'contents'
-            regex: "(?i)sauce.{0,50}(\\\"|'|`)?[0-9a-f-]{36}(\\\"|'|`)?"
-            name: 'Sauce Token'
-            - part: 'contents'
-            regex: '(xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})'
-            name: 'Slack Token'
-            - part: 'contents'
-            regex: 'https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}'
-            name: 'Slack Webhook'
-            - part: 'contents'
-            regex: "(?i)sonar.{0,50}(\\\"|'|`)?[0-9a-f]{40}(\\\"|'|`)?"
-            name: 'SonarQube Docs API Key'
-            - part: 'contents'
-            regex: "(?i)hockey.{0,50}(\\\"|'|`)?[0-9a-f]{32}(\\\"|'|`)?"
-            name: 'HockeyApp'
-            - part: 'contents'
-            regex: '([\w+]{1,24})(://)([^$<]{1})([^\s";]{1,}):([^$<]{1})([^\s";]{1,})@[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,24}([^\s]+)'
-            name: 'Username and password in URI'
-            - part: 'contents'
-            regex: 'oy2[a-z0-9]{43}'
-            name: 'NuGet API Key'
-
-
-            // other lists
+            /*
             [Nett.TomlIgnore]
             public string[] ShareStringsToPrioritise { get; set; } =
             {

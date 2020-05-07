b3fff1354341e02582d62011aa4ae6c267a33274
Mike Loss <mike@mikeloss.net>
2020-05-07 06:16:38 -0700

    working on config file gen

--- SnaffCore/Classifiers/ClassifierRule.cs
+++ SnaffCore/Classifiers/ClassifierRule.cs
@@ -20,7 +20,7 @@ namespace Classifiers
         public MatchLoc MatchLocation { get; set; } = MatchLoc.FileName;
         public MatchListType WordListType { get; set; } = MatchListType.Contains;
         public List<string> WordList { get; set; } = new List<string>();
-        public List<Regex> Regexes { get; set; } = new List<Regex>();
+        public List<Regex> Regexes { get; set; }
 
         // define the severity of any matches
         public Triage Triage { get; set; } = Triage.Black;

269c1a60d2e1f6fedb541278b1a05e74299630d3
l0ss <mike@mikeloss.net>
2021-05-17 08:01:42 +0800

    Update Options.cs

--- SnaffCore/Config/Options.cs
+++ SnaffCore/Config/Options.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace SnaffCore.Config
 {
@@ -13,10 +13,10 @@ namespace SnaffCore.Config
         public bool ScanNetlogon { get; set; } = true;
 
         // Concurrency Options
-        public int MaxThreads { get; set; } = 30;
-        public int ShareThreads { get; set; }
-        public int TreeThreads { get; set; }
-        public int FileThreads { get; set; }
+        //public int MaxThreads { get; set; } = 30;
+        public int ShareThreads { get; set; } = 30;
+        public int TreeThreads { get; set; } = 20;
+        public int FileThreads { get; set; } = 50;
         public int MaxFileQueue { get; set; } = 200000;
         public int MaxTreeQueue { get; set; } = 0;
         public int MaxShareQueue { get; set; } = 0;
@@ -55,7 +55,7 @@ namespace SnaffCore.Config
         };
 
         // this sets the maximum size of file to look inside.
-        public long MaxSizeToGrep { get; set; } = 10000000;
+        public long MaxSizeToGrep { get; set; } = 1000000;
 
         // these enable or disable automated downloading of files that match the criteria
         public bool Snaffle { get; set; } = false;
@@ -67,9 +67,6 @@ namespace SnaffCore.Config
 
         public Options()
         {
-            ShareThreads = MaxThreads / 2;
-            TreeThreads = MaxThreads / 4;
-            FileThreads = MaxThreads / 4;
             //PrepareClassifiers();
             //BuildDefaultClassifiers();
         }

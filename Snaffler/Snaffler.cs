e84defd9ad1a3d5120082eb879cabd629b3ee11c
l0ss <mike@mikeloss.net>
2021-07-02 08:18:48 +0800

    fixed up some noisy rules, made Main() public so can load with ps reflection

--- Snaffler/Snaffler.cs
+++ Snaffler/Snaffler.cs
@@ -2,9 +2,9 @@
 
 namespace Snaffler
 {
-    internal static class Snaffler
+    public static class Snaffler
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             SnaffleRunner runner = new SnaffleRunner();
             runner.Run(args);

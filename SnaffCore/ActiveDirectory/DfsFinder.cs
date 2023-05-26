4d469bcca3896f54916c6399a4516c5bcbcbee7a
Kelley <8828244+hkelley@users.noreply.github.com>
2023-05-26 15:22:22 -0400

    changed DFSv2 (2008 mode) share path finder

--- SnaffCore/ActiveDirectory/DfsFinder.cs
+++ SnaffCore/ActiveDirectory/DfsFinder.cs
@@ -115,7 +115,7 @@ namespace SnaffCore.ActiveDirectory
 
                     var target_list = resEnt.GetPropertyAsBytes(@"msdfs-targetlistv2");
                     var xml = new XmlDocument();
-                    string thing = System.Text.Encoding.Unicode.GetString(target_list.Skip(2).Take(target_list.Length - 1 + 1 - 2).ToArray());
+                    //string thing = System.Text.Encoding.Unicode.GetString(target_list.Skip(2).Take(target_list.Length - 1 + 1 - 2).ToArray());
                     xml.LoadXml(System.Text.Encoding.Unicode.GetString(target_list.Skip(2).Take(target_list.Length - 1 + 1 - 2).ToArray()));
 
                     if (xml.FirstChild != null)
@@ -126,11 +126,17 @@ namespace SnaffCore.ActiveDirectory
                             {
                                 try
                                 {
-                                    var Target = node.InnerText;
+                                    var Target = babbynode.InnerText;
                                     if (Target.Contains(@"\"))
                                     {
                                         var DFSroot = Target.Split('\\')[3];
-                                        string ShareName = resEnt.GetProperty(@"msdfs-linkpathv2").Replace("/","\\");
+                                                                                
+                                        string ShareName = resEnt.GetProperty(@"msdfs-linkpathv2").Replace("/","\\");                                        
+                                        if(ShareName == "\\ITPDocs")
+                                        {
+                                            Console.WriteLine("Found {0}", ShareName);
+                                        }
+
                                         // FIX  DFS V2 shares have the share name in the DFSroot, don't double-up
                                         DFSShares.Add(new DFSShare {
                                             Name = $@"{DFSroot}",

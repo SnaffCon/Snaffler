255949093a2078d5f4712df0092c091fc1b871e2
mloss <mloss@go.away>
2025-10-07 02:41:14 +0000

    fix for when you pass a domain but not a DC, or when you pass a DC as an FQDN instead of an IP

--- SnaffCore/ActiveDirectory/Helpers.cs
+++ SnaffCore/ActiveDirectory/Helpers.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SnaffCore.ActiveDirectory.LDAP
 {
     class Helpers
     {
-        private static readonly HashSet<string> Groups = new HashSet<string> { "268435456", "268435457", "536870912", "536870913" };
+
+    private static readonly HashSet<string> Groups = new HashSet<string> { "268435456", "268435457", "536870912", "536870913" };
         private static readonly HashSet<string> Computers = new HashSet<string> { "805306369" };
         private static readonly HashSet<string> Users = new HashSet<string> { "805306368" };
         internal static LdapTypeEnum SamAccountTypeToType(string samAccountType)
